1e1349a5c332ad96fd7dca97616e48527d880d12
tahere fahimi <t.fahimi1998@gmail.com>
2020-06-17 01:12:34 +0430

    some changes added

--- FCM.py
+++ FCM.py
@@ -6,28 +6,31 @@ import math
 from matplotlib import pyplot as plt
 
 
-def initializeMembershipMatrix():
-    membership_mat = list()
+# assign value randomly to the fuzzy value quantities
+def initialize_fuzzy_quantities():
+    matrix = list()
     for i in range(n):
-        random_num_list = [random.random() for i in range(k)]
-        summation = sum(random_num_list)
-        temp_list = [x / summation for x in random_num_list]
-        membership_mat.append(temp_list)
-    return membership_mat
+        temp_list = [random.random() for i in range(c)]
+        summation = sum(temp_list)
+        # normalize the fuzzy quantities values
+        matrix.append([x / summation for x in temp_list])
+    return matrix
 
 
-def calculateClusterCenter(membership_mat):
-    cluster_mem_val = [[], [], [], []]
+def find_Centers(u, c):
+    cluster_quantity = []
+    # create a list to save the fuzzy quantities for each center
+    for i in range(c):
+        cluster_quantity.append([])
+    #  separate data in each cluster
     for s in range(n):
-        cluster_mem_val[0].append(membership_mat[s][0])
-        cluster_mem_val[1].append(membership_mat[s][1])
-        cluster_mem_val[2].append(membership_mat[s][2])
-        cluster_mem_val[3].append(membership_mat[s][3])
+        for i in range(c):
+            cluster_quantity[i].append(u[s][i])
 
     cluster_centers = list()
-    for j in range(k):
-        # x = list(cluster_mem_val[j])
-        x = cluster_mem_val[j]
+    # check this part ...............................................
+    for j in range(c):
+        x = cluster_quantity[j]
         xraised = [e ** m for e in x]
         denominator = sum(xraised)
         temp_num = list()
@@ -41,39 +44,38 @@ def calculateClusterCenter(membership_mat):
     return cluster_centers
 
 
-def updateMembershipValue(membership_mat, cluster_centers):
+def updateMembershipValue(u, cluster_centers):
     p = float(2 / (m - 1))
     for i in range(n):
-        x = list(df.iloc[i])
-        # distances = [np.linalg.norm(map(operator.sub, x, cluster_centers[j])) for j in range(k)]
-        distances = [np.linalg.norm(list(map(operator.sub, x, cluster_centers[j]))) for j in range(k)]
-        for j in range(k):
-            den = sum([math.pow(float(distances[j] / distances[c]), p) for c in range(k)])
-            membership_mat[i][j] = float(1 / den)
-    return membership_mat
+        x_y = list(df.iloc[i])
+        distances = [np.linalg.norm(list(map(operator.sub, x_y, cluster_centers[j]))) for j in range(c)]
+        for j in range(c):
+            den = sum([math.pow(float(distances[j] / distances[k]), p) for k in range(c)])
+            u[i][j] = float(1 / den)
+    return u
 
 
-def getClusters(membership_mat):
+def change_clusters(u):
     cluster_labels = list()
     for i in range(n):
-        max_val, idx = max((val, idx) for (idx, val) in enumerate(membership_mat[i]))
+        max_val, idx = max((val, idx) for (idx, val) in enumerate(u[i]))
         cluster_labels.append(idx)
     return cluster_labels
 
 
-def fuzzyCMeansClustering():
-    # Membership Matrix
-    membership_mat = initializeMembershipMatrix()
-    curr = 0
-    while curr <= MAX_ITER:
-        cluster_centers = calculateClusterCenter(membership_mat)
-        membership_mat = updateMembershipValue(membership_mat, cluster_centers)
-        cluster_labels = getClusters(membership_mat)
-        curr += 1
-    print(membership_mat)
-    return cluster_labels, cluster_centers
+def fcm(c):
+    # u matrix => the matrix of the fuzzy quantities
+    u = initialize_fuzzy_quantities()
+    iteration_number = 0
+    while iteration_number <= maximum_iteration:
+        centers = find_Centers(u, c)
+        u = updateMembershipValue(u, centers)
+        labels = change_clusters(u)
+        iteration_number += 1
+    return labels
 
 
+# draw the final vision of the coordinates
 def plot(labels):
     colors = ["r", "b", "g", "y"]
     for i in range(n):
@@ -86,32 +88,28 @@ def plot(labels):
             plt.scatter(array[0], array[1], color=colors[2])
         else:
             plt.scatter(array[0], array[1], color=colors[3])
-
     # plt.savefig('.png')
     plt.show()
 
 
-
-if __name__=="__main__":
-    # read file and initate the values
-    df_full = pd.read_csv("sample2.csv")
+if __name__ == "__main__":
+    # read file and initiate the values
+    df_full = pd.read_csv("sample3.csv")
     columns = list(df_full.columns)
     features = columns[:len(columns)]
     # print(features)
     class_labels = list(df_full[columns[-1]])
     df = df_full[features]
     print("the first coordinate", list(df.iloc[0]))
-    # Number of Attributes
-    attribute = len(df.columns)
     # Number of Clusters
-    k = 4
+    c = 4
     # Maximum number of iterations
-    MAX_ITER = 10
+    maximum_iteration = 10
     # Number of data points
     n = len(df)
     # Fuzzy parameter
-    m = 2.00
+    m = 1.80
 
-    labels, centers = fuzzyCMeansClustering()
+    labels = fcm(c)
     print("the labels are", labels)
     plot(labels)
